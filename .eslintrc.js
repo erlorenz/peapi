3152901bb142b154725ff898f108a506f0ab9f56
erlorenz <38394390+erlorenz@users.noreply.github.com>
2019-03-27 09:37:29 -0700

    fixed eslint and prettier to work with typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,12 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     'prettier',
     'prettier/@typescript-eslint',
-],
+  ],
     plugins: ["@typescript-eslint", "prettier"],
     rules: {
         "prettier/prettier": "error",
         "@typescript-eslint/camelcase": 0,
         "@typescript-eslint/explicit-member-accessibility": 0,
+        "@typescript-eslint/no-parameter-properties": 0,
       },
   };
